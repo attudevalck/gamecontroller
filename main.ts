a108e27479ad83534cd16cc8fb95f80acc15ecdb
attudevalck <maximknockaert@gmail.com>
2025-02-03 14:11:32 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left) || Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right)) {
+        basic.showString("1")
+        basic.pause(500)
+        basic.clearScreen()
+    } else {
+        basic.clearScreen()
+    }
+})
